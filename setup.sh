1cc3937a167cf7d7dc29a5f8073ecf6b45ba7681
Alex Navasardyan <twokul@users.noreply.github.com>
2022-03-15 12:17:25 -0400

    Add "keep alive" and "sudo" commands

--- setup.sh
+++ setup.sh
@@ -51,6 +51,13 @@ clone_dotfiles() {
   fi
 }
 
+# Might as well ask for password up-front, right?
+echo "Starting install script, please grant me sudo access..."
+sudo -v
+
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Automatically install Homebrew if it is not installed
 install_homebrew || echo "Failed to install Homebrew"
 

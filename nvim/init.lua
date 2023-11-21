89e43e50b546a891bbfdb77db489eb80ca6f19d8
Alex Navasardyan <alex.navasardyan@gmail.com>
2023-11-21 08:56:52 -0600

    Switch fonts

--- nvim/init.lua
+++ nvim/init.lua
@@ -100,7 +100,7 @@ require("lazy").setup({
   },
 
   --  https://github.com/nvim-tree/nvim-tree.lua
-   {
+  {
     "nvim-tree/nvim-tree.lua",
     version = "*",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
